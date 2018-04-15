054d17a141cdad87927d20ee7c9f8e02c64808eb
Certificate <valtteri.vuori@live.fi>
2018-04-15 18:32:12 +0300

    .

--- JavaCode/src/Main.java
+++ JavaCode/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
 
 
-
     public static void main(String args[]) {
         System.out.println("Hello! Connecting to DB...");
         ConnectionHandler connection = new ConnectionHandler("tiko", "t1k0");
