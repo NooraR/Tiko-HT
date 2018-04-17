b025a1eb4039fc533256f7ddbb88a6286188b97c
Defrosted <ermurj@hotmail.com>
2018-04-17 16:27:31 +0300

    Fixed issues including a issue with names being reserved by psql.

--- luontilauseet.sql
+++ luontilauseet.sql
@@ -1,44 +1,45 @@
-CREATE TABLE user (
-    id INT SERIAL,
+CREATE TABLE useraccount (
+    id SERIAL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     address VARCHAR(50),
-    email VARCHAR(50),
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(300),
     phone_number VARCHAR(20),
     PRIMARY KEY(id)
 );
 
 CREATE TABLE antiquarian (
-    id INT SERIAL,
-    name VARCHAR(50) NOT NULL,
+    id      SERIAL,
+    name    VARCHAR(50) NOT NULL,
     address VARCHAR(50),
-    web VARCHAR(50),
-    PRIMARY KEY(tunnus)
+    web     VARCHAR(50),
+    PRIMARY KEY (id)
 );
 
 CREATE TYPE order_status AS ENUM (
     'waiting', 'processed', 'sent'
- );
+);
 
-CREATE TABLE order (
-    id INT SERIAL,
-    orderdate DATE NOT NULL,
-    status order_status,
-    user INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(user) REFERENCES user(id)
+CREATE TABLE userorder (
+    id          SERIAL,
+    orderdate   DATE NOT NULL,
+    status      order_status DEFAULT 'waiting',
+    userid      INT  NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userid) REFERENCES useraccount (id)
 );
 
 CREATE TABLE work (
-    id INT SERIAL,
-    auchtor VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    isbn VARCHAR(20),
+    id        SERIAL,
+    auchtor   VARCHAR(50) NOT NULL,
+    name      VARCHAR(50) NOT NULL,
+    isbn      VARCHAR(20),
     published INT,
-    genre VARCHAR(50),
-    type VARCHAR(50),
-    weight DECIMAL NOT NULL,
-    PRIMARY KEY(id)
+    genre     VARCHAR(50),
+    type      VARCHAR(50),
+    weight    DECIMAL     NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TYPE product_status AS ENUM (
@@ -46,13 +47,13 @@ CREATE TYPE product_status AS ENUM (
 );
 
 CREATE TABLE product (
-    id INT SERIAL,
-    isbn VARCHAR(20) NOT NULL,
-    order INT,
-    status product_status DEFAULT 'free',
-    selling_price DECIMAL,
+    id             SERIAL,
+    workid         INT NOT NULL,
+    orderid        INT,
+    status         product_status DEFAULT 'free',
+    selling_price  DECIMAL,
     purchase_price DECIMAL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(isbn) REFERENCES work(isbn),
-    FOREIGN KEY(order) REFERENCES order(id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (workid) REFERENCES work (id),
+    FOREIGN KEY (orderid) REFERENCES userorder (id)
 );
