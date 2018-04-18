68caebec1811bffbfcacce279c3a0397299bb42d
Defrosted <ermurj@hotmail.com>
2018-04-18 14:09:06 +0300

    Moved database query functions into their own class. Need to figure out how to pass the user and password to the jdbc in ConnectionHandler.

--- src/main/java/ConnectionHandler.java
+++ src/main/java/ConnectionHandler.java
@@ -1,8 +1,4 @@
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-
-import datamodel.*;
 
 class ConnectionHandler {
 
@@ -17,7 +13,7 @@ class ConnectionHandler {
             this.password = password;
     }
 
-    public Connection connect() throws ClassNotFoundException {
+    public Connection getConnection() throws ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         Connection connection = null;
         try {
@@ -39,73 +35,4 @@ class ConnectionHandler {
             System.err.println("Error while closing connection.");
         }
     }
-
-    public List<User> getAllUsers() throws SQLException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        List<User> userList = null;
-
-        try {
-            connection = connect();
-            ps = connection.prepareStatement("SELECT * FROM userAccount;");
-            rs = ps.executeQuery();
-
-            userList = new ArrayList<User>();
-            while(rs.next()) {
-                User user = new User();
-                user.setId(rs.getInt("id"));
-                user.setFirstName(rs.getString("first_name"));
-                user.setLastName(rs.getString("last_name"));
-                user.setAddress(rs.getString("address"));
-                user.setEmail(rs.getString("email"));
-                user.setPhoneNumber(rs.getString("phone_number"));
-
-                userList.add(user);
-            }
-
-            return userList;
-        } catch(Exception e) {
-            System.err.println("Error while generating List<User>!");
-        } finally {
-            closeConnection(connection, ps, rs);
-        }
-        return null;
-    }
-
-    public List<Work> getAllWorks() throws SQLException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        List<Work> workList = null;
-
-        try {
-            connection = connect();
-            ps = connection.prepareStatement("SELECT * FROM useraccount;");
-            rs = ps.executeQuery();
-
-            workList = new ArrayList<Work>();
-            while(rs.next()) {
-                Work work = new Work();
-                work.setId(rs.getInt("id"));
-                work.setAuthor(rs.getString("author"));
-                work.setName(rs.getString("name"));
-                work.setIsbn(rs.getString("isbn"));
-                work.setPublished(rs.getInt("published"));
-                work.setGenre(rs.getString("genre"));
-                work.setType(rs.getString("type"));
-                work.setWeight(rs.getDouble("weight"));
-
-                workList.add(work);
-            }
-
-            return workList;
-        } catch(Exception e) {
-            System.err.println("Error while generating List<User>!");
-        } finally {
-            closeConnection(connection, ps, rs);
-        }
-        return null;
-    }
-
 }
---
+++ src/main/java/QueryHandler.java
@@ -0,0 +1,85 @@
+import datamodel.User;
+import datamodel.Work;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryHandler {
+    ConnectionHandler con;
+
+    public QueryHandler() {
+        con = new ConnectionHandler();
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<User> userList = null;
+
+        try {
+            connection = con.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM userAccount;");
+            rs = ps.executeQuery();
+
+            userList = new ArrayList<User>();
+            while(rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setFirstName(rs.getString("first_name"));
+                user.setLastName(rs.getString("last_name"));
+                user.setAddress(rs.getString("address"));
+                user.setEmail(rs.getString("email"));
+                user.setPhoneNumber(rs.getString("phone_number"));
+
+                userList.add(user);
+            }
+
+            return userList;
+        } catch(Exception e) {
+            System.err.println("Error while generating List<User>!");
+        } finally {
+            con.closeConnection(connection, ps, rs);
+        }
+        return null;
+    }
+
+    public List<Work> getAllWorks() throws SQLException {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Work> workList = null;
+
+        try {
+            connection = con.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM useraccount;");
+            rs = ps.executeQuery();
+
+            workList = new ArrayList<Work>();
+            while(rs.next()) {
+                Work work = new Work();
+                work.setId(rs.getInt("id"));
+                work.setAuthor(rs.getString("author"));
+                work.setName(rs.getString("name"));
+                work.setIsbn(rs.getString("isbn"));
+                work.setPublished(rs.getInt("published"));
+                work.setGenre(rs.getString("genre"));
+                work.setType(rs.getString("type"));
+                work.setWeight(rs.getDouble("weight"));
+
+                workList.add(work);
+            }
+
+            return workList;
+        } catch(Exception e) {
+            System.err.println("Error while generating List<User>!");
+        } finally {
+            con.closeConnection(connection, ps, rs);
+        }
+        return null;
+    }
+}
