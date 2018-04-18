adde75b3c66dc286c2c9c3a6ddb35826e910b661
Defrosted <ermurj@hotmail.com>
2018-04-18 15:30:23 +0300

    Fixed some errors.

--- sql/lisayslauseet.sql
+++ sql/lisayslauseet.sql
@@ -12,31 +12,31 @@ INSERT INTO antiquarian VALUES
 
 -- users
 
-INSERT INTO user VALUES
+INSERT INTO useraccount VALUES
     ('Pertti', 'Pääkäyttäjä', 'Pitkämäenkatu 17, 33270 Tampere', 'pertti.paakayttaja@sapo.fi', '00000000000');
 
-INSERT INTO user VALUES
+INSERT INTO useraccount VALUES
     ('Maija', 'Meikäläinen', 'Pitkämäenkatu 12, 33270 Tampere', 'maija.meikalainen@sapo.fi', '00000000000');
 
 
 -- Teokset
 
-INSERT INTO product VALUES
+INSERT INTO work VALUES
     ('Madeleine Brent', 'Elektran tytär', 9155430674, 1986, 'romantiikka', 'romaani', 1.2);
 
-INSERT INTO product VALUES
+INSERT INTO work VALUES
     ('Madeleine Brent', 'Tuulentavoittelijan morsian', 9156381451, 1978, 'romantiikka', 'romaani', 0,8);
 
-INSERT INTO product VALUES
+INSERT INTO work VALUES
     ('Mika Waltari', 'Turms kuolematon', NULL,1995, 'historia', 'romaani', 1.4);
 
 
-INSERT INTO product VALUES
+INSERT INTO work VALUES
     ('Mika Waltari', 'Komisario Palmun erehdys', NULL, 1940, 'dekkari', 'romaani', 0.8);
 
-INSERT INTO product VALUES
+INSERT INTO work VALUES
     ('Shelton Gilbert', 'Friikkilän pojat Mexicossa', NULL, 1989, 'huumori', 'sarjakuva', 0.3);
 
-INSERT INTO product VALUES
+INSERT INTO work VALUES
     ('Dale Carnegien', 'Miten saan ystäviä, menestystä, vaikutusvaltaa', 9789510396230, 1939, 'opas', 'tietokirja', 1.9);
 
