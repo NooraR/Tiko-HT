61c793a675a91a293c0dbf505d379786c5d7dce2
Defrosted <ermurj@hotmail.com>
2018-04-21 18:59:35 +0300

    Implemented a method for adding new users.

--- src/main/java/database/UserHandler.java
+++ src/main/java/database/UserHandler.java
@@ -4,6 +4,7 @@ import datamodel.User;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 public class UserHandler {
     private SessionFactory sessionFactory;
@@ -28,4 +29,25 @@ public class UserHandler {
         }
     }
 
+    public int addUser(User user) throws Exception {
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+            Query query = session.createQuery("from User where email=:email");
+            query.setParameter("email", user.getEmail());
+
+            int userId = -1;
+            if(query.uniqueResult() == null) {
+                userId = (Integer) session.save(user);
+            }
+
+            session.getTransaction().commit();
+
+            return userId;
+        } catch (HibernateException e) {
+            session.getTransaction().rollback();
+            throw new Exception("Failed to add new user: " + e.getMessage());
+        }
+    }
 }
