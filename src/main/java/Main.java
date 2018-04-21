6fd1b931a1bbbb407b3f1399076f1c0282c9fe36
Defrosted <ermurj@hotmail.com>
2018-04-21 18:00:19 +0300

    Hibernate test.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import database.UserHandler;
+import datamodel.User;
+
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        try {
+            UserHandler handler = new UserHandler();
+            User user = handler.getUserById(1);
+
+            System.out.println("Hei " + user.getFirstName() + " " + user.getLastName() + "!");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
