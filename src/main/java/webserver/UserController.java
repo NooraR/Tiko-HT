aa6e22928ddd17aadd498d158f9a6a4be91e55ff
Defrosted <ermurj@hotmail.com>
2018-04-21 22:15:44 +0300

    Finished register function implementation.

--- src/main/java/webserver/UserController.java
+++ src/main/java/webserver/UserController.java
@@ -1,16 +1,21 @@
 package webserver;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import database.UserHandler;
 import datamodel.User;
 import org.hibernate.SessionFactory;
 import spark.Request;
 import spark.Response;
 
+import javax.persistence.EntityExistsException;
+
 public class UserController {
     public static String register(Request req, Response res, SessionFactory sessionFactory) {
         Gson gson = new Gson();
         String json = null;
+
+        res.type("application/json");
 
         try {
             UserHandler handler = new UserHandler(sessionFactory);
@@ -18,17 +23,24 @@ public class UserController {
             User user = gson.fromJson(req.body(), User.class);
             handler.addUser(user);
 
-            json = gson.toJson(new Object() {
-                boolean success = true;
-                String username = user.getEmail();
-            });
+            json = "{" +
+                        "\"success\": true, " +
+                        "\"email\": \"" + user.getEmail() + "\"" +
+                    "}";
+            res.status(200);
+        } catch (EntityExistsException e) {
+            json = "{" +
+                        "\"success\": false, " +
+                        "\"error\": \"Email is already registered\"" +
+                    "}";
+            res.status(400);
         } catch (Exception e) {
-            json = gson.toJson(new Object() {
-               boolean success = false;
-               String error = "Failed to add user";
-            });
-
-            System.err.println("Error while creating new user from json:" + req.body() + "\nError: " + e.getMessage());
+            json = "{" +
+                        "\"success\": false, " +
+                        "\"error\": \"Sorry, could not register user.\"" +
+                    "}";
+            System.err.println("Error while creating new user from json: " + e.getMessage());
+            res.status(400);
         }
         return json;
     }
