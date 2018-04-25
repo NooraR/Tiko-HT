52f0ad72a7e9cf7a4f9d5d5a18cd1a9f924196c1
Certificate <valtteri.vuori@live.fi>
2018-04-25 18:16:36 +0300

    Added user role checking

--- src/main/java/webserver/controllers/ManagementController.java
+++ src/main/java/webserver/controllers/ManagementController.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import database.ProductHandler;
 import datamodel.Product;
+import datamodel.User;
 import org.hibernate.SessionFactory;
 import spark.Request;
 import spark.Response;
@@ -31,6 +32,23 @@ public class ManagementController {
             res.status(400);
             System.err.println("Failed to add a new product" + e.getMessage());
             return gson.toJson(new Reply(false, "Failed to add the product", null));
+        }
+    }
+
+    public static String checkUserPermissions(Request req, Response res){
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        try{
+            if (req.session() != null && req.session().attribute("user") != null){
+                User user = req.session().attribute("user");
+                String msg = "User "+user.getFirstName()+" "+user.getLastName()+" admin status: "+user.isAdmin();
+                System.out.println("Permission checked succesfully Result = "+user.isAdmin());
+                return gson.toJson(new Reply(user.isAdmin(), msg, null));
+            }
+            else
+                throw new Exception("Either the session or the user is null");
+        } catch (Exception e){
+            System.err.println("Exception when checking permission " + e.getMessage());
+            return gson.toJson(new Reply(false, "Failed to check user permission.", null));
         }
     }
 }
